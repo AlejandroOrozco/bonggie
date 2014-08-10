28b932655a459aff53c2015e7dc5e64c87cfa66b
Alejandro Orozco Builes <Alejandro@MacBook-Pro-de-Alejandro.local>
2014-08-10 17:46:38 -0500

    Added Users and widgets

--- js/app/config/routes.js
+++ js/app/config/routes.js
@@ -1,4 +1,7 @@
 bonggie.config(['$routeProvider','$httpProvider',function ($routeProvider,$httpProvider) {
 	$routeProvider.when('/', {templateUrl: 'js/app/partials/views/dashboard.html'});
+	$routeProvider.when('/users', {templateUrl: 'js/app/partials/views/users.html'});
+	$routeProvider.when('/profile', {templateUrl: 'js/app/partials/views/profile.html'});
+	$routeProvider.when('/widgets', {templateUrl: 'js/app/partials/views/widgets.html'});
 	$routeProvider.otherwise({ redirectTo: '/' });
 }]);
